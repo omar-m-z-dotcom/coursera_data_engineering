d15a14224a81de028252dcc490b5545f76e8586f
omar mohamed <triro765@gmail.com>
2024-07-24 18:43:50 +0300

    packging

---
+++ mymath/geometry.py
@@ -0,0 +1,9 @@
+import math
+
+def area_of_rectangle(length, breadth):
+    """Calculate the area of a rectangle given its length and breadth."""
+    return length * breadth
+
+def area_of_circle(radius):
+    """Calculate the area of a circle given its radius."""
+    return math.pi * radius * radius
